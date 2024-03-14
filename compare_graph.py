bad1af39d6e911cc0450443753920d8bbe05a0cc
GbotemiB <gbotemibolarinwa@gmail.com>
2024-03-14 14:35:18 +0100

    removed comments

--- compare_graph.py
+++ compare_graph.py
@@ -49,21 +49,3 @@ def plot_network_graph(network: pypsa.Network, seed: int = 1969):
     pos = nx.spring_layout(graph, seed=seed)  # Seed for reproducible layout
     nx.draw(graph, pos, **options)
     plt.savefig("network_graph.png", dpi=300)
-
-
-if __name__ == "__main__":
-
-    # network_path = "elec_s_37.nc"
-
-    # # load network
-    # network = pypsa.Network(network_path)
-    # network_properties = graph_properties(network)
-    # print(f"edges: {network_properties['network_edges']}")
-    # print(f"nodes: {network_properties['network_nodes']}")
-    # print(
-    #     f"degree of connectedness: {network_properties['network_average_degree_connectivity']}")
-    # print(f"degree of network: {network_properties['network_degree']}")
-
-    # # plot network graph
-    # plot_network_graph(network)
-    pass
