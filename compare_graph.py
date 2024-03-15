3d326894ff96d0bece5f3b763974a26c6796d979
GbotemiB <gbotemibolarinwa@gmail.com>
2024-03-15 13:50:40 +0100

    formatted document

--- compare_graph.py
+++ compare_graph.py
@@ -61,7 +61,9 @@ def plot_network_graph(network: pypsa.Network, seed: int = 1969) -> None:
     plt.savefig("network_graph.png", dpi=300)
 
 
-def available_item_properties(network1: pypsa.Network, network2: pypsa.Network, item: str) -> list:
+def available_item_properties(
+    network1: pypsa.Network, network2: pypsa.Network, item: str
+) -> list:
     """
     Identifies items present in both network1 and network2 for the specified item type.
 
@@ -76,7 +78,8 @@ def available_item_properties(network1: pypsa.Network, network2: pypsa.Network, 
 
     if item not in ["network_nodes", "network_edges"]:  # Validate item type
         raise ValueError(
-            "Invalid item type. Must be 'network_nodes' or 'network_edges'.")
+            "Invalid item type. Must be 'network_nodes' or 'network_edges'."
+        )
 
     available_item = []  # Initialize a list to store common items
 
@@ -88,7 +91,9 @@ def available_item_properties(network1: pypsa.Network, network2: pypsa.Network, 
     return available_item
 
 
-def missing_item_properties(network1: pypsa.Network, network2: pypsa.Network, item: str) -> list:
+def missing_item_properties(
+    network1: pypsa.Network, network2: pypsa.Network, item: str
+) -> list:
     """
     Identifies items present in network1 but missing in network2 for the specified item type.
 
@@ -103,7 +108,8 @@ def missing_item_properties(network1: pypsa.Network, network2: pypsa.Network, it
 
     if item not in ["network_nodes", "network_edges"]:  # Validate item type
         raise ValueError(
-            "Invalid item type. Must be 'network_nodes' or 'network_edges'.")
+            "Invalid item type. Must be 'network_nodes' or 'network_edges'."
+        )
 
     missing_item = []  # Initialize a list to store missing items
 
@@ -133,36 +139,43 @@ def compare_graph_properties(network1: pypsa.Network, network2: pypsa.Network) -
         network2)  # Get properties of network 2
 
     difference_dict = {}  # Dictionary to store property differences
-    similar_dict = {}     # Dictionary to store property similarities
+    similar_dict = {}  # Dictionary to store property similarities
 
-    results = {
-        "similarities": similar_dict,
-        "differences": difference_dict
-    }
+    results = {"similarities": similar_dict, "differences": difference_dict}
 
     # Find common properties (edges & nodes) present in both networks
     similar_dict["edges"] = available_item_properties(
-        network1_properties, network2_properties, "network_edges")
+        network1_properties, network2_properties, "network_edges"
+    )
     similar_dict["nodes"] = available_item_properties(
-        network1_properties, network2_properties, "network_nodes")
+        network1_properties, network2_properties, "network_nodes"
+    )
 
     # Calculate the absolute difference in the number of edges and nodes
     difference_dict["edge_difference"] = abs(
-        network1_properties["network_number_of_edges"] - network2_properties["network_number_of_edges"])
+        network1_properties["network_number_of_edges"]
+        - network2_properties["network_number_of_edges"]
+    )
     difference_dict["node_difference"] = abs(
-        network1_properties["network_number_of_nodes"] - network2_properties["network_number_of_nodes"])
+        network1_properties["network_number_of_nodes"]
+        - network2_properties["network_number_of_nodes"]
+    )
 
     # Find missing edges (those in network 1 but not in network 2 and vice versa)
     edges_in_n1_not_in_n2 = missing_item_properties(
-        network1_properties, network2_properties, "network_edges")
+        network1_properties, network2_properties, "network_edges"
+    )
     edges_in_n2_not_in_n1 = missing_item_properties(
-        network2_properties, network1_properties, "network_edges")
+        network2_properties, network1_properties, "network_edges"
+    )
 
     # Find missing nodes (those in network 1 but not in network 2 and vice versa)
     nodes_in_n1_not_in_n2 = missing_item_properties(
-        network1_properties, network2_properties, "network_nodes")
+        network1_properties, network2_properties, "network_nodes"
+    )
     nodes_in_n2_not_in_n1 = missing_item_properties(
-        network2_properties, network1_properties, "network_nodes")
+        network2_properties, network1_properties, "network_nodes"
+    )
 
     difference_dict["edges_in_n1_not_in_n2"] = edges_in_n1_not_in_n2
     difference_dict["edges_in_n2_not_in_n1"] = edges_in_n2_not_in_n1
@@ -172,19 +185,26 @@ def compare_graph_properties(network1: pypsa.Network, network2: pypsa.Network) -
     degree = {}  # Dictionary to store degree information
 
     # Calculate average degree for each network
-    average_network_degree = pd.DataFrame([network1_properties["network_average_degree_connectivity"],
-                                           network2_properties["network_average_degree_connectivity"]], index=["network_1", "network_2"]).T
+    average_network_degree = pd.DataFrame(
+        [
+            network1_properties["network_average_degree_connectivity"],
+            network2_properties["network_average_degree_connectivity"],
+        ],
+        index=["network_1", "network_2"],
+    ).T
     degree["average_degree"] = average_network_degree.sort_index(inplace=True)
 
     # Combine degree information for both networks into a DataFrame
     network_degree = pd.DataFrame(
-        [network1_properties["network_degree"], network2_properties["network_degree"]]).T
+        [network1_properties["network_degree"],
+            network2_properties["network_degree"]]
+    ).T
     network_degree.rename(
         columns={"0": "network_1", "1": "network_2"}, inplace=True)
 
     difference_dict["degree"] = {
         "network_degree": network_degree,
-        "average_network_degree": average_network_degree
+        "average_network_degree": average_network_degree,
     }
 
     return results
